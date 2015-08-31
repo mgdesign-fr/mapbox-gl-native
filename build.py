1f66f2a4ab68049fbda6459c7399b011c5c549c8
Jean-Francois Verdon <jf.verdon@mgdesign.fr>
2015-08-31 18:04:32 +0200

    Comment about gdb crash

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ import os, subprocess
 
 UNIT_TESTS=None
 UNIT_TESTS = ["fixture_log_observer.cpp", "main.cpp", "mock_file_source.cpp", "util.cpp", "gtest-all.cc"]
+# "glyph_store.cpp" and "thread.cpp" > crash gdb
 UNIT_TESTS += ["geo.cpp", "glyph_store.cpp", "sprite_atlas.cpp", "sprite_image.cpp", "sprite_parser.cpp", "sprite_store.cpp", "thread.cpp", "work_queue.cpp"]
 BUILD_RENDER_EXE = UNIT_TESTS is None
 LINK_MAPBOX_GL_DLL = UNIT_TESTS is None
