3fb036c1a20e5516971df0c663dbef07ab79977c
Jean-Francois Verdon <jf.verdon@mgdesign.fr>
2015-08-31 16:12:36 +0200

    Patching thread test for mswindows platform

--- test/miscellaneous/thread.cpp
+++ test/miscellaneous/thread.cpp
@@ -5,6 +5,21 @@
 
 using namespace mbgl::util;
 
+#ifdef WIN32
+#include <windows.h>
+void usleep(int waitTime)
+{
+    __int64 time1 = 0, time2 = 0, freq = 0;
+    QueryPerformanceCounter((LARGE_INTEGER *) &time1);
+    QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
+
+    do
+    {
+        QueryPerformanceCounter((LARGE_INTEGER *) &time2);
+    } while((time2-time1) < waitTime);
+}
+#endif
+
 class TestObject {
 public:
     TestObject(std::thread::id otherTid)
