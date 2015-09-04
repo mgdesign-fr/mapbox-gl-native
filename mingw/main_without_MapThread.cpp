3f8debbf2dc501c38032a1b425dc160611aeb12f
Nicolas Lelong <nicolas.lelong@mgdesign.fr>
2015-09-04 10:15:17 +0200

    mingw/main_without_MapThread: first attempt, will need cleanup

---
+++ mingw/main_without_MapThread.cpp
@@ -0,0 +1,249 @@
+#include <mbgl/mbgl.hpp>
+#include "../platform/default/default_styles.hpp"
+#include <mbgl/util/uv.hpp>
+#include <mbgl/platform/log.hpp>
+#include <mbgl/platform/platform.hpp>
+#include <mbgl/platform/default/settings_json.hpp>
+#include <mbgl/platform/default/glfw_view.hpp>
+#include <mbgl/storage/default_file_source.hpp>
+#include <mbgl/storage/sqlite_cache.hpp>
+
+#include <mbgl/map/map_context.hpp>
+#include <mbgl/map/map_data.hpp>
+#include <mbgl/map/transform.hpp>
+
+#include <signal.h>
+#include <getopt.h>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+
+namespace {
+
+std::unique_ptr<GLFWView> view;
+
+}
+
+void quit_handler(int) {
+    if (view) {
+        mbgl::Log::Info(mbgl::Event::Setup, "waiting for quit...");
+        view->setShouldClose();
+    } else {
+        exit(0);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    bool fullscreen = false;
+    bool benchmark = false;
+    std::string style;
+    double latitude = 0, longitude = 0;
+    double bearing = 0, zoom = 1;
+    bool skipConfig = false;
+
+    const struct option long_options[] = {
+        {"fullscreen", no_argument, 0, 'f'},
+        {"benchmark", no_argument, 0, 'b'},
+        {"style", required_argument, 0, 's'},
+        {"lon", required_argument, 0, 'x'},
+        {"lat", required_argument, 0, 'y'},
+        {"zoom", required_argument, 0, 'z'},
+        {"bearing", required_argument, 0, 'r'},
+        {0, 0, 0, 0}
+    };
+
+    while (true) {
+        int option_index = 0;
+        int opt = getopt_long(argc, argv, "fbs:", long_options, &option_index);
+        if (opt == -1) break;
+        switch (opt)
+        {
+        case 0:
+            if (long_options[option_index].flag != 0)
+                break;
+        case 'f':
+            fullscreen = true;
+            break;
+        case 'b':
+            benchmark = true;
+            break;
+        case 's':
+            style = std::string("asset://") + std::string(optarg);
+            break;
+        case 'x':
+            longitude = atof(optarg);
+            skipConfig = true;
+            break;
+        case 'y':
+            latitude = atof(optarg);
+            skipConfig = true;
+            break;
+        case 'z':
+            zoom = atof(optarg);
+            skipConfig = true;
+            break;
+        case 'r':
+            bearing = atof(optarg);
+            skipConfig = true;
+            break;
+        default:
+            break;
+        }
+
+    }
+
+    if (benchmark) {
+        mbgl::Log::Info(mbgl::Event::General, "BENCHMARK MODE: Some optimizations are disabled.");
+    }
+
+    view = std::make_unique<GLFWView>(fullscreen, benchmark);
+
+    mbgl::SQLiteCache cache("/tmp/mbgl-cache.db");
+    mbgl::DefaultFileSource fileSource(&cache);
+
+    // Set access token if present
+    const char *token = getenv("MAPBOX_ACCESS_TOKEN");
+    if (token == nullptr) {
+        mbgl::Log::Warning(mbgl::Event::Setup, "no access token set. mapbox.com tiles won't work.");
+    } else {
+        fileSource.setAccessToken(std::string(token));
+    }
+
+    //mbgl::Map map(*view, fileSource);
+
+    enum class RenderState {
+        never,
+        partial,
+        fully
+    };
+
+    RenderState renderState = RenderState::never;
+
+    uv::loop l;
+    mbgl::util::RunLoop loop(l.get());
+
+    mbgl::util::MapThreadContextRegistrar fakeMapThread;
+    mbgl::MapData mapData(mbgl::MapMode::Continuous, 1.0f);
+    mbgl::MapContext mapContext(*view, fileSource, mapData);
+    mbgl::Transform transform(*view);
+
+    transform.resize(view->getSize());
+    mapContext.triggerUpdate(transform.getState(), mbgl::Update::Dimensions);
+
+    //mapData.setDebug(true);
+    /*
+    // Load settings
+    mbgl::Settings_JSON settings;
+
+    if (skipConfig) {
+        map.setLatLngZoom(mbgl::LatLng(latitude, longitude), zoom);
+        map.setBearing(bearing);
+        mbgl::Log::Info(mbgl::Event::General, "Location: %f/%f (z%.2f, %.2f deg)", latitude, longitude, zoom, bearing);
+    } else {
+        map.setLatLngZoom(mbgl::LatLng(settings.latitude, settings.longitude), settings.zoom);
+        map.setBearing(settings.bearing);
+        map.setDebug(settings.debug);
+    }
+    */
+
+    view->setChangeStyleCallback([&mapContext] () {
+        static uint8_t currentStyleIndex;
+
+        if (++currentStyleIndex == mbgl::util::defaultStyles.size()) {
+            currentStyleIndex = 0;
+        }
+
+        const auto& newStyle = mbgl::util::defaultStyles[currentStyleIndex];
+        mapContext.setStyleURL(newStyle.first);
+        view->setWindowTitle(newStyle.second);
+
+        mbgl::Log::Info(mbgl::Event::Setup, std::string("Changed style to: ") + newStyle.first);
+    });
+
+    // Load style
+    if (style.empty()) {
+        const auto& newStyle = mbgl::util::defaultStyles[1];
+        style = newStyle.first;
+        view->setWindowTitle(newStyle.second);
+    }
+
+    mapContext.setStyleURL(style);
+
+    zoom = 5.0;
+
+    //view->run();
+    while (!glfwWindowShouldClose(view->window)) {
+        
+        //glfwWaitEvents();
+        glfwPollEvents();
+        uv_run(l.get(), UV_RUN_NOWAIT);
+
+        const bool dirty = true; //!view->clean.test_and_set();
+
+        longitude += 1e-3;
+        transform.setLatLngZoom(mbgl::LatLng{latitude, longitude}, zoom, mbgl::CameraOptions());
+        mapContext.triggerUpdate(transform.getState(), mbgl::Update::Zoom);
+        
+        if (dirty) {
+            const double started = glfwGetTime();
+            
+            //map->renderSync();
+            if (renderState == RenderState::never) {
+                view->notifyMapChange(mbgl::MapChangeWillStartRenderingMap);
+            }
+
+            view->notifyMapChange(mbgl::MapChangeWillStartRenderingFrame);
+
+            const bool fullyLoaded = mapContext.renderSync(transform.getState(), mbgl::FrameData { view->getFramebufferSize() });
+
+            view->notifyMapChange(fullyLoaded ?
+                mbgl::MapChangeDidFinishRenderingFrameFullyRendered :
+                mbgl::MapChangeDidFinishRenderingFrame);
+
+            if (!fullyLoaded) {
+                renderState = RenderState::partial;
+            } else if (renderState != RenderState::fully) {
+                renderState = RenderState::fully;
+                view->notifyMapChange(mbgl::MapChangeDidFinishRenderingMapFullyRendered);
+            }
+
+            //TODO//report(1000 * (glfwGetTime() - started));
+
+            if (benchmark) {
+                //map->setNeedsRepaint();
+                mapData.setNeedsRepaint(true);
+            }
+
+            //map->nudgeTransitions();
+            mbgl::Update flags = transform.updateTransitions(mbgl::Clock::now());
+            if (mapData.getNeedsRepaint()) {
+                flags |= mbgl::Update::Repaint;
+            }
+            if (flags & mbgl::Update::Dimensions) {
+                transform.resize(view->getSize());
+            }
+            mapContext.triggerUpdate(transform.getState(), flags);
+        }
+    }
+
+    mapContext.cleanup();
+
+    /*
+    // Save settings
+    mbgl::LatLng latLng = transform.getLatLng();
+    settings.latitude = latLng.latitude;
+    settings.longitude = latLng.longitude;
+    settings.zoom = transform.getZoom();
+    settings.bearing = transform.getBearing();
+    settings.debug = mapData.getDebug();
+    if (!skipConfig) {
+        settings.save();
+    }
+    mbgl::Log::Info(mbgl::Event::General,
+                    "Exit location: --lat=\"%f\" --lon=\"%f\" --zoom=\"%f\" --bearing \"%f\"",
+                    settings.latitude, settings.longitude, settings.zoom, settings.bearing);
+    */
+
+    return 0;
+}
