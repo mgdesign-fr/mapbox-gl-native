1e87461256ba32e352f9bfb50a4bfbd663c59449
Nicolas Lelong <nicolas.lelong@mgdesign.fr>
2015-09-04 14:52:24 +0200

    main_without_MapThread: uses a CApiView instead of GLFWView (which crashed due to the lack of 'Map' instance)

--- mingw/main_without_MapThread.cpp
+++ mingw/main_without_MapThread.cpp
@@ -4,11 +4,13 @@
 #include <mbgl/platform/log.hpp>
 #include <mbgl/platform/platform.hpp>
 #include <mbgl/platform/default/settings_json.hpp>
-#include <mbgl/platform/default/glfw_view.hpp>
 #include <mbgl/storage/default_file_source.hpp>
 #include <mbgl/storage/sqlite_cache.hpp>
 
+#include <mbgl/platform/default/glfw_view.hpp>          // only to include GLFW
 #include <mbgl/c_api/map_immediate.hpp>
+#include <mbgl/mbgl_c.h>
+#include <mbgl/c_api/c_api_view.hpp>
 
 #include <signal.h>
 #include <getopt.h>
@@ -19,19 +21,64 @@
 
 /*****************************************************************************/
 
+struct GLFWdata {
+    GLFWwindow* window;
+    int dirty;
+    uint16_t width, height;
+    uint16_t fbWidth, fbHeight;
+};
+
+float getPixelRatio(mbgl_CApiView_t* view, void* userdata) {
+    return 1.0;
+}
+
+void getSize(mbgl_CApiView_t* view, void* userdata, uint16_t* width, uint16_t* height) {
+    GLFWdata* data = (GLFWdata*)userdata;
+    *width = data->width;
+    *height = data->height;
+}
+
+void getFramebufferSize(mbgl_CApiView_t* view, void* userdata, uint16_t* width, uint16_t* height) {
+    GLFWdata* data = (GLFWdata*)userdata;
+    *width = data->fbWidth;
+    *height = data->fbHeight;
+}
+
+void activate(mbgl_CApiView_t* view, void* userdata) {
+    GLFWdata* data = (GLFWdata*)userdata;
+    glfwMakeContextCurrent(data->window);    
+}
+
+void deactivate(mbgl_CApiView_t* view, void* userdata) {
+    glfwMakeContextCurrent(NULL);    
+}
+
+void notify(mbgl_CApiView_t* view, void* userdata) {
+    glfwPostEmptyEvent();
+}
+
+void invalidate(mbgl_CApiView_t* view, void* userdata) {
+    GLFWdata* data = (GLFWdata*)userdata;
+    data->dirty = 1;
+    glfwPostEmptyEvent();
+}
+
+void swap(mbgl_CApiView_t* view, void* userdata) {
+    GLFWdata* data = (GLFWdata*)userdata;
+    glfwSwapBuffers(data->window);
+}
+
+/*****************************************************************************/
+
 namespace {
 
-std::unique_ptr<GLFWView> view;
+int shouldCloseWindow = 0;
 
 }
 
 void quit_handler(int) {
-    if (view) {
-        mbgl::Log::Info(mbgl::Event::Setup, "waiting for quit...");
-        view->setShouldClose();
-    } else {
-        exit(0);
-    }
+    mbgl::Log::Info(mbgl::Event::Setup, "waiting for quit...");
+    shouldCloseWindow = 1;
 }
 
 int main(int argc, char *argv[]) {
@@ -93,11 +140,102 @@ int main(int argc, char *argv[]) {
 
     }
 
+    /**/
+
+    GLFWdata viewData = {};
+
+    /**/
+
+    if (!glfwInit()) {
+        mbgl::Log::Error(mbgl::Event::OpenGL, "failed to initialize glfw");
+        exit(1);
+    }
+
+    int width=1024, height=768;
+    GLFWmonitor *monitor = nullptr;
+    if (fullscreen) {
+        monitor = glfwGetPrimaryMonitor();
+        auto videoMode = glfwGetVideoMode(monitor);
+        width = videoMode->width;
+        height = videoMode->height;
+    }
+
+#ifdef DEBUG
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+#endif
+    glfwWindowHint(GLFW_RED_BITS, 8);
+    glfwWindowHint(GLFW_GREEN_BITS, 8);
+    glfwWindowHint(GLFW_BLUE_BITS, 8);
+    glfwWindowHint(GLFW_ALPHA_BITS, 8);
+    glfwWindowHint(GLFW_STENCIL_BITS, 8);
+    glfwWindowHint(GLFW_DEPTH_BITS, 16);
+    GLFWwindow* window = glfwCreateWindow(width, height, "Mapbox GL", monitor, NULL);
+    if (!window) {
+        glfwTerminate();
+        mbgl::Log::Error(mbgl::Event::OpenGL, "failed to initialize window");
+        exit(1);
+    }
+
+    glfwSetWindowUserPointer(window, &viewData);
+    glfwMakeContextCurrent(window);
+    if (benchmark) {
+        // Disables vsync on platforms that support it.
+        glfwSwapInterval(0);
+    } else {
+        glfwSwapInterval(1);
+    }
+
+    glfwSetWindowSizeCallback(window, [](GLFWwindow *window, int width, int height) {
+        GLFWdata *view = reinterpret_cast<GLFWdata *>(glfwGetWindowUserPointer(window));
+        view->width = width;
+        view->height = height;
+        view->dirty = 1;
+    });
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow *window, int width, int height) {
+        GLFWdata *view = reinterpret_cast<GLFWdata *>(glfwGetWindowUserPointer(window));
+        view->fbWidth = width;
+        view->fbHeight = height;
+        view->dirty = 1;
+    });
+
+    mbgl::gl::InitializeExtensions(glfwGetProcAddress);
+
+    glfwGetWindowSize(window, &width, &height);
+    int fbWidth, fbHeight;
+    glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
+    float pixelRatio = static_cast<float>(fbWidth) / width;
+
+    glfwMakeContextCurrent(nullptr);
+
+    /**/    
+
+    viewData.window = window;
+    viewData.width = width;
+    viewData.height = height;
+    viewData.fbWidth = fbWidth;
+    viewData.fbHeight = fbHeight;
+
+    mbgl_CApiView_Callbacks_t viewFunctions = {
+        getPixelRatio,
+        getSize,
+        getFramebufferSize,
+        activate,
+        deactivate,
+        notify,
+        invalidate,
+        swap
+    };
+
+    mbgl_CApiView_t* view_c = NULL;
+    mbgl_CApiView_init(&viewFunctions, &viewData, &view_c);
+
+    mbgl::CApiView view(view_c);
+
+    /**/    
+
     if (benchmark) {
         mbgl::Log::Info(mbgl::Event::General, "BENCHMARK MODE: Some optimizations are disabled.");
     }
-
-    view = std::make_unique<GLFWView>(fullscreen, benchmark);
 
     mbgl::SQLiteCache cache("cache.sqlite");
     mbgl::DefaultFileSource fileSource(&cache);
@@ -110,106 +248,46 @@ int main(int argc, char *argv[]) {
         fileSource.setAccessToken(std::string(token));
     }
 
-    //mbgl::Map map(*view, fileSource);
-
-    MapThreadContext imMapThreadContext;                                    // IMPORTANT(nico) must be created before MapContext (implicit dependency)
+    mbgl::MapThreadContext imMapThreadContext;                                    // IMPORTANT(nico) must be created before MapContext (implicit dependency)
 
     mbgl::MapData mapData(mbgl::MapMode::Continuous, 1.0f);
-    mbgl::MapContext mapContext(*view, fileSource, mapData);
-    mbgl::Transform transform(*view);
+    mbgl::MapContext mapContext(view, fileSource, mapData);
+    mbgl::Transform transform(view);
 
-    MapImmediate imMap(&mapData, &mapContext, &transform);
-    imMap.resize(view.get());
-
-    //mapData.setDebug(true);
-    /*
-    // Load settings
-    mbgl::Settings_JSON settings;
-
-    if (skipConfig) {
-        map.setLatLngZoom(mbgl::LatLng(latitude, longitude), zoom);
-        map.setBearing(bearing);
-        mbgl::Log::Info(mbgl::Event::General, "Location: %f/%f (z%.2f, %.2f deg)", latitude, longitude, zoom, bearing);
-    } else {
-        map.setLatLngZoom(mbgl::LatLng(settings.latitude, settings.longitude), settings.zoom);
-        map.setBearing(settings.bearing);
-        map.setDebug(settings.debug);
-    }
-    */
-
-    view->setChangeStyleCallback([&mapContext] () {
-        static uint8_t currentStyleIndex;
-
-        if (++currentStyleIndex == mbgl::util::defaultStyles.size()) {
-            currentStyleIndex = 0;
-        }
-
-        const auto& newStyle = mbgl::util::defaultStyles[currentStyleIndex];
-        mapContext.setStyleURL(newStyle.first);
-        view->setWindowTitle(newStyle.second);
-
-        mbgl::Log::Info(mbgl::Event::Setup, std::string("Changed style to: ") + newStyle.first);
-    });
+    mbgl::MapImmediate imMap(&mapData, &mapContext, &transform);
+    imMap.resize(&view);
 
     // Load style
     if (style.empty()) {
         const auto& newStyle = mbgl::util::defaultStyles[1];
         style = newStyle.first;
-        view->setWindowTitle(newStyle.second);
     }
 
     mapContext.setStyleURL(style);
 
-    //view->run();
-    while (!glfwWindowShouldClose(view->window)) {
+    while (!(shouldCloseWindow || glfwWindowShouldClose(window))) {
         
-        //glfwWaitEvents();
         glfwPollEvents();
         imMapThreadContext.process();
 
-        const bool dirty = true; //!view->clean.test_and_set();
+        if (viewData.dirty) {
+            imMap.resize(&view);
+            viewData.dirty = 0;
+        }
 
         longitude += 1e-3;
         zoom = 5.0 + 0.25 * cos(longitude * 8.0) - 2.0 * sin(longitude * 1.33);
-
         transform.setLatLngZoom(mbgl::LatLng{latitude, longitude}, zoom, mbgl::CameraOptions());
         mapContext.triggerUpdate(transform.getState(), mbgl::Update::Zoom);
         
-        if (dirty) {
-            const double started = glfwGetTime();
-            
-            //map->renderSync();
-            imMap.render(view.get());
-
-            //TODO//report(1000 * (glfwGetTime() - started));
-
-            if (benchmark) {
-                //map->setNeedsRepaint();
-                mapData.setNeedsRepaint(true);
-            }
-
-            //map->nudgeTransitions();
-            imMap.update();
-        }
+        imMap.render(&view);
+        imMap.update();
     }
 
     mapContext.cleanup();
-
-    /*
-    // Save settings
-    mbgl::LatLng latLng = transform.getLatLng();
-    settings.latitude = latLng.latitude;
-    settings.longitude = latLng.longitude;
-    settings.zoom = transform.getZoom();
-    settings.bearing = transform.getBearing();
-    settings.debug = mapData.getDebug();
-    if (!skipConfig) {
-        settings.save();
-    }
-    mbgl::Log::Info(mbgl::Event::General,
-                    "Exit location: --lat=\"%f\" --lon=\"%f\" --zoom=\"%f\" --bearing \"%f\"",
-                    settings.latitude, settings.longitude, settings.zoom, settings.bearing);
-    */
+    
+    glfwDestroyWindow(viewData.window);
+    glfwTerminate();
 
     return 0;
 }
